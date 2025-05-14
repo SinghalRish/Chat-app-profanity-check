58138c609d0c93f44b616b78555abaab743c96c5
SinghalRish <singhalrishabh18@gmail.com>
2025-05-14 14:20:24 +0530

    styling in jsx style

--- frontend/src/components/AuthImagePattern.jsx
+++ frontend/src/components/AuthImagePattern.jsx
@@ -1,6 +1,7 @@
 const AuthImagePattern = ({ title, subtitle }) => {
   return (
-    <div className="hidden lg:flex items-center justify-center bg-base-200 p-12" style="margin-top: 50px">
+    <div className="hidden lg:flex items-center justify-center bg-base-200 p-12" style={{ marginTop: "50px" }}
+>
       <div className="max-w-md text-center">
         <div className="grid grid-cols-3 gap-3 mb-8">
           {[...Array(9)].map((_, i) => (
